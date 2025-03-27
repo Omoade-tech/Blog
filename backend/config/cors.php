78c483fac8c5b6e598ce66f350192814a6fff915
Adedoyin Sodiq <sodiqadedoyin12@gmail.com>
2025-03-27 12:46:12 +0100

    login fix 1

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -17,7 +17,7 @@ return [
 
    'paths' => ['api/*', 'sanctum/csrf-cookie'], 
     'allowed_methods' => ['*'], 
-    'allowed_origins' => ['https://blog-post-aorf.onrender.com'], 
+    'allowed_origins' => ['https://blog-post-aorf.onrender.com', 'https://blogpost-db.onrender.com', 'http://localhost:3000', 'http://localhost:5173'], 
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'], 
     'exposed_headers' => ['*'],
